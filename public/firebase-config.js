6f76eefe8ccd87613f9201909134895daf4eeb33
Alexander Shubin <alorian@yandex.ru>
2024-03-04 11:41:57 +0500

    feat: Firebase config added

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAluIHGGQ2U87Ngt8jtCECqZSy4V0qN2Jc",
+  authDomain: "voxmessaging.firebaseapp.com",
+  projectId: "voxmessaging",
+  storageBucket: "voxmessaging.appspot.com",
+  messagingSenderId: "621070090053",
+  appId: "1:621070090053:web:7182714c021b2dd1d5c82c",
+  measurementId: "G-HPZEVFNR4L",
+};
