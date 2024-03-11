4a9d70d922395895faf4eb3728aee7bb715eb682
Alexander Shubin <alorian@yandex.ru>
2024-03-12 02:55:45 +0500

    ci: Adjust commitlint configuration to limit body line length
    The commitlint configuration has been updated to introduce a line length limit for commit message bodies. This change enforces a maximum line length of 300, to ensure clear and concise commit messages while still allowing for necessary detail.

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "subject-case": [2, "always", ["sentence-case"]],
+    "body-max-line-length": [2, "always", 300],
   },
 };
